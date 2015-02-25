5526bbc9dd27951958460ea96a7f642e4d0eede0
saisoft <saisoft@php-linux>
2015-02-25 00:45:02 -0500

    Fresh start

---
+++ HelloWord.php
@@ -0,0 +1,21 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+?>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        $str1 = "Hello";
+        $str2 = "World";
+        echo "$str1 $str2";
+        ?>
+    </body>
+</html>
